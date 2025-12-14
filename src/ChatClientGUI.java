100c3706faf8a4941d709c7d7528b06de97200d3
chera1606 <cherinetderbie@gmail.com>
2025-12-14 14:43:32 +0300

    Add JTextArea to display chat messages

--- src/ChatClientGUI.java
+++ src/ChatClientGUI.java
@@ -3,8 +3,12 @@ import javax.swing.*;
 public class ChatClientGUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Chat App");
+        JTextArea chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        frame.add(chatArea);
         frame.setSize(400, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
+
