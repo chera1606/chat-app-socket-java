e7f22800b019afa0613005afde9701ae96a59f78
chera1606 <cherinetderbie@gmail.com>
2025-12-14 15:01:41 +0300

    Refactor ChatClient main method socket initialization

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -8,7 +8,6 @@ import java.net.*;
 public class ChatClient {
     public static void main(String[] args) {
         try (Socket socket = new Socket("localhost", 5000)) {
-
             BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
