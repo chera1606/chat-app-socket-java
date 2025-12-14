7c9934f1931b0dc55d0cccc3492809dbbd234ef6
chera1606 <cherinetderbie@gmail.com>
2025-12-14 14:44:14 +0300

    Add input field and Send button to GUI

--- src/ChatClientGUI.java
+++ src/ChatClientGUI.java
@@ -5,10 +5,23 @@ public class ChatClientGUI {
         JFrame frame = new JFrame("Chat App");
         JTextArea chatArea = new JTextArea();
         chatArea.setEditable(false);
+
+        JTextField inputField = new JTextField();
+        JButton sendButton = new JButton("Send");
+
+        frame.setLayout(null);
+        chatArea.setBounds(10, 10, 360, 400);
+        inputField.setBounds(10, 420, 260, 30);
+        sendButton.setBounds(280, 420, 90, 30);
+
         frame.add(chatArea);
+        frame.add(inputField);
+        frame.add(sendButton);
+
         frame.setSize(400, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
+
 
