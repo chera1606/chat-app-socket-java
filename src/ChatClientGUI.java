4c9d7bf960d880b22597040c9b1082c5d07a3bac
chera1606 <cherinetderbie@gmail.com>
2025-12-14 14:42:23 +0300

    Create basic GUI window

---
+++ src/ChatClientGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ChatClientGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Chat App");
+        frame.setSize(400, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
